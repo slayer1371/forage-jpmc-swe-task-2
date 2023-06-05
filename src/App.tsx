90f2300e3796898c02410ed2d9b5e8d881ee5af7
Mrinal <mrinal,sharma2020@vitstudent.vit.ac.in>
2023-06-05 11:21:06 +0530

    Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import './App.css';
 /**
  * State declaration for <App />
  */
-interface IState {
+interface IState { 
   data: ServerRespond[],
+  showGraph: boolean, // add a new state variable to indicate whether the graph should be shown
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, // initialize the state variable to false
     };
   }
 
@@ -29,6 +31,7 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showGraph)
     return (<Graph data={this.state.data}/>)
   }
 
@@ -36,11 +39,17 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+    let x=0;
+    const interval = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
     });
+    x++;
+    if(x>1000)
+    clearInterval(interval);
+    }, 100);
   }
 
   /**
